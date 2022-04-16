2b51ded5534e450d78f25692dff3a6592c07181d
YongTing <jiangyongting0810@gmail.com>
2022-04-16 15:26:28 +0800

    +css

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ var server = http.createServer(function(request, response){
     <html lang="en">
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="/x">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
         <title>Document</title>
